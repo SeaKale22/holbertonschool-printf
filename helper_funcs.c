f9c8793b22495af0cf9ce9dc8545d7f15bec5e10
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-07-08 09:42:21 -0700

    updated char_comp to compare a string to a char

--- helper_funcs.c
+++ helper_funcs.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * char_comp - compares two characters
- * @c1: char 1
+ * char_comp - compares string to a char
+ * @c1: string of a single char
  * @c2: char 2
  * Return: 0 if equal and 1 if not
  */
 
-int char_comp(char c1, char c2)
+int char_comp(char *c1, char c2)
 {
-	if (c1 == c2)
+	if (c1[0] == c2)
 		return (0);
 	else
 		return (1);
